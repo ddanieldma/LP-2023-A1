a733e692168bd78a62af7c4b728642dbbff148b9
guilierm <cguilherme182@gmail.com>
2023-10-09 18:55:40 -0300

    add função ler_csv

---
+++ read.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def ler_csv(caminho_csv):
+    return pd.read_csv(caminho_csv, encoding='latin-1', sep = ';')
+
+base_inep = ler_csv("ed-superior-inep.csv")
+
+print(base_inep.head())
