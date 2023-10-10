37ac6c24da970fdabfd4b98bf6da801a6b39ec27
guilierm <cguilherme182@gmail.com>
2023-10-09 21:04:49 -0300

    add função para plotar mapa

--- read.py
+++ read.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import geopandas as gpd
 
 def ler_csv(caminho_csv):
     return pd.read_csv(caminho_csv, encoding='latin-1', sep = ';')
 
-base_inep = ler_csv("ed-superior-inep.csv")
-
-print(base_inep.head())
+def criar_geometria_brasil():
+    geometria_brasil = gpd.read_file("bcim_2016_21_11_2018.gpkg", layer = "lim_unidade_federacao_a")
+    return geometria_brasil
