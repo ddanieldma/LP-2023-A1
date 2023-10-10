951f3263f3c911ee88a5742bda63c032030c073d
guilierm <cguilherme182@gmail.com>
2023-10-09 21:10:17 -0300

    add tratamento para plot

--- read.py
+++ read.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
-
 
 def ler_csv(caminho_csv):
     return pd.read_csv(caminho_csv, encoding='latin-1', sep = ';')
 
-geometria_brasil = gpd.read_file("bcim_2016_21_11_2018.gpkg", layer = "lim_unidade_federacao_a")
-
-geometria_brasil.plot()
-plt.show()
+def criar_geometria_brasil():
+    geometria_brasil = gpd.read_file("bcim_2016_21_11_2018.gpkg", layer = "lim_unidade_federacao_a")
+    return geometria_brasil
--- tratamento_base.py
+++ tratamento_base.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import geopandas as gpd
 from read import ler_csv
+from read import criar_geometria_brasil
+import matplotlib.pyplot as plt
 
 df_guilherme = ler_csv("ed-superior-inep.csv")
+df_gui_copia = df_guilherme.copy()
+df_gui_copia.rename({"SG_UF_IES": "sigla"}, axis = 1, inplace = True)
+df_para_plot = df_gui_copia.groupby("sigla")["QT_DOC_EXE"].sum().reset_index()
 
-print(df_guilherme.head())
+geometria_brasil = criar_geometria_brasil()
+
+dataframe_plot = geometria_brasil.merge(df_para_plot, on = "sigla", how = "left")
