45361ada925659f323c43f8a02bb7b1c48ba3f1f
guilierm <cguilherme182@gmail.com>
2023-10-09 21:38:03 -0300

    pequena alteração estética no plot

--- plot_guilherme.py
+++ plot_guilherme.py
@@ -3,6 +3,9 @@ from tratamento_base import dataframe_plot
 import matplotlib.pyplot as plt
 
 dataframe_plot.plot(column = "QT_DOC_EXE",
-                    cmap = "Reds",
+                    cmap = "viridis",
                     legend = True)
+
+plt.title("Número de Docentes de Ensino Superior por Estado ", fontsize=11)
+
 plt.show()
