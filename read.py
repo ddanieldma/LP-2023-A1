7f480393ac6d6c1543a8666bb7f6d032b8554271
ddanieldma <danielalmemiranda@gmail.com>
2023-10-10 15:45:34 -0300

    lembrete

--- read.py
+++ read.py
@@ -3,6 +3,10 @@ import pandas as pd
 def ler_csv(caminho_csv):
     return pd.read_csv(caminho_csv, encoding='latin-1', sep = ';')
 
+"""
+Adicionar tratamento de exceção
+"""
+
 base_inep = ler_csv("ed-superior-inep.csv")
 
 print(base_inep.head())
