875218d15a5096992e2c2dec3cda8a29ca33fc9f
guilierm <cguilherme182@gmail.com>
2023-10-11 12:29:34 -0300

    organização em funcões

--- plot_guilherme.py
+++ plot_guilherme.py
@@ -1,11 +1,19 @@
 import geopandas as gpd
-from tratamento_base import dataframe_plot
+from tratamento_base import tratar_base
 import matplotlib.pyplot as plt
 
-dataframe_plot.plot(column = "QT_DOC_EXE",
+def customização_plot():
+    plt.title("Número de Docentes de Ensino Superior por Estado ", fontsize=11)
+    return True
+
+def plotar_gráfico():
+    dataframe_plot = tratar_base()
+    dataframe_plot.plot(column = "QT_DOC_EXE",
                     cmap = "viridis",
                     legend = True)
+    
+    customização_plot()
 
-plt.title("Número de Docentes de Ensino Superior por Estado ", fontsize=11)
+    return plt.show()
 
-plt.show()
+plotar_gráfico()
--- tratamento_base.py
+++ tratamento_base.py
@@ -4,11 +4,14 @@ from read import ler_csv
 from read import criar_geometria_brasil
 import matplotlib.pyplot as plt
 
-df_guilherme = ler_csv("ed-superior-inep.csv")
-df_gui_copia = df_guilherme.copy()
-df_gui_copia.rename({"SG_UF_IES": "sigla"}, axis = 1, inplace = True)
-df_para_plot = df_gui_copia.groupby("sigla")["QT_DOC_EXE"].sum().reset_index()
+def tratar_base():
+    df_guilherme = ler_csv("ed-superior-inep.csv")
+    df_gui_copia = df_guilherme.copy()
+    df_gui_copia.rename({"SG_UF_IES": "sigla"}, axis = 1, inplace = True)
+    df_para_plot = df_gui_copia.groupby("sigla")["QT_DOC_EXE"].sum().reset_index()
 
-geometria_brasil = criar_geometria_brasil()
+    geometria_brasil = criar_geometria_brasil()
 
-dataframe_plot = geometria_brasil.merge(df_para_plot, on = "sigla", how = "left")
+    dataframe_plot = geometria_brasil.merge(df_para_plot, on = "sigla", how = "left")
+
+    return dataframe_plot
