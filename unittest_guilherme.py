1b9ee981a2558b0d7fdf9ec1d54dddfd3ef31ae8
guilierm <cguilherme182@gmail.com>
2023-10-12 12:00:29 -0300

    add unittest 3

--- unittest_guilherme.py
+++ unittest_guilherme.py
@@ -8,47 +8,56 @@ import matplotlib.pyplot as plt
 from tratamento_base import *
 
 #criando dataframes para serem utilizados nos testes
-dados_teste_1 = {"RJ": [1, 2, 3], "SP": [4, 5, 6]}
-df_teste_1 = pd.DataFrame(dados_teste_1)
+dados_teste_renomear_1 = {"RJ": [1, 2, 3], "SP": [4, 5, 6]}
+df_teste_renomear_1 = pd.DataFrame(dados_teste_renomear_1)
 
-dados_teste_2 = {"RJ": [1, 2, 3], "MG": [4, 5, 6]}
-df_teste_2 = pd.DataFrame(dados_teste_2)
+dados_teste_renomear_2 = {"RJ": [1, 2, 3], "MG": [4, 5, 6]}
+df_teste_renomear_2 = pd.DataFrame(dados_teste_renomear_2)
 
-dados_teste_3 = {"BARBACENA": [1, 2, 3], "MG": [4, 5, 6]}
-df_teste_3 = pd.DataFrame(dados_teste_3)
+dados_teste_agrupar_1 = {"sigla": ["SP", "RJ", "SP", "RJ"], "QT_DOC_EXE": [100, 200, 150, 250]}
+df_teste_agrupar_1 = pd.DataFrame(dados_teste_agrupar_1)
 
-dados_teste_4 = {"sigla": ["SP", "RJ", "SP", "RJ"], "QT_DOC_EXE": [100, 200, 150, 250]}
-df_teste_4 = pd.DataFrame(dados_teste_4)
+dados_teste_agrupar_2 = {"sigla": ["RJ", "SP"], "QT_DOC_EXE": [450, 250]}
+df_teste_agrupar_2 = pd.DataFrame(dados_teste_agrupar_2)
 
-dados_teste_4 = {"sigla": ["SP", "RJ", "SP", "RJ"], "QT_DOC_EXE": [100, 200, 150, 250]}
-df_teste_4 = pd.DataFrame(dados_teste_4)
-
-dados_teste_5 = {"sigla": ["RJ", "SP"], "QT_DOC_EXE": [450, 250]}
-df_teste_5 = pd.DataFrame(dados_teste_5)
+dados_teste_merge_1 = {"ID": [1, 2, 3], "Estados": ["MG", "RJ", "SP"]}
+df_teste_merge_1 = pd.DataFrame(dados_teste_merge_1)
+dados_teste_merge_2 = {"ID": [1, 2, 3], "Área": [50000, 10000, 70000]}
+df_teste_merge_2 = pd.DataFrame(dados_teste_merge_2)
+dados_teste_merge_3 = {"ID": [1, 2, 3], "Estados": ["MG", "RJ", "SP"], "Área": [50000, 10000, 70000]}
+df_teste_merge_3 = pd.DataFrame(dados_teste_merge_3)
 
 class TesteFunções(unittest.TestCase):
 
     def test_função_renomear(self):
         #Não se espera erro
-        base_renomeada = renomear_coluna(df_teste_1, "SP", "MG")
-        self.assertTrue(base_renomeada.equals(df_teste_2))
+        base_renomeada = renomear_coluna(df_teste_renomear_1, "SP", "MG")
+        self.assertTrue(base_renomeada.equals(df_teste_renomear_2))
 
     def test_renomear_coluna_2(self):
         #Erro esperado
-        base_renomeada = renomear_coluna(df_teste_1, "SP", "MG")
-        self.assertTrue(base_renomeada.equals(df_teste_3))
+        base_renomeada = renomear_coluna(df_teste_renomear_1, "SP", "MG")
+        self.assertTrue(base_renomeada.equals(df_teste_agrupar_1))
 
     def test_agrup_dados(self):
         #Não se espera erro
-        base_agrupada = agrupamento_de_dados(df_teste_4, "sigla", "QT_DOC_EXE")
-        self.assertTrue(base_agrupada.equals(df_teste_5))
+        base_agrupada = agrupamento_de_dados(df_teste_agrupar_1, "sigla", "QT_DOC_EXE")
+        self.assertTrue(base_agrupada.equals(df_teste_agrupar_2))
 
     def test_agrup_dados_2(self):
-        #Não se espera erro
-        base_agrupada = agrupamento_de_dados(df_teste_4, "sigla", "QT_DOC_EXE")
-        self.assertTrue(base_agrupada.equals(df_teste_3))
+        #Erro esperado
+        base_agrupada = agrupamento_de_dados(df_teste_agrupar_1, "sigla", "QT_DOC_EXE")
+        self.assertTrue(base_agrupada.equals(df_teste_renomear_2))
 
-    
+    def test_merge_bases(self):
+        #Não se espera erro
+        base_resultado = merge_bases(df_teste_merge_1, df_teste_merge_2, "ID")
+        self.assertTrue(base_resultado.equals(df_teste_merge_3))
+
+    def test_merge_bases_2(self):
+        #Erro esperado
+        base_resultado = merge_bases(df_teste_merge_1, df_teste_merge_2, "ID")
+        self.assertTrue(base_resultado.equals(df_teste_renomear_2))
 
 if __name__ == "__main__":
     unittest.main()
