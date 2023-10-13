9a4bafb50e62f417e1b3f62d444f1d0239166ec4
guilierm <cguilherme182@gmail.com>
2023-10-13 15:31:47 -0300

    add type hint e docstring funções read

--- read.py
+++ read.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import geopandas as gpd
+from pandas import DataFrame
+from geopandas import GeoDataFrame
 
-def ler_csv(caminho_csv): 
+def ler_csv(caminho_csv) -> DataFrame: 
+    """Recebe um arquivo csv e retona um dataframe
 
+    :param str caminho_csv: caminho do arquivo csv
+
+    :return: DataFrame produzido com base no arquivo csv
+    :rtype: pandas.DataFrame
+    """
     try:
         df = pd.read_csv(caminho_csv, encoding='latin-1', sep = ';')
     except FileNotFoundError:
@@ -16,7 +24,14 @@ def ler_csv(caminho_csv):
 base_inep = ler_csv("ed-superior-inep.csv")
 
 
-def criar_geometria_brasil():
+def criar_geometria_brasil() -> GeoDataFrame:
+    """Recebe um arquivo e retona um geodataframe
+
+    :param str caminho_csv: caminho do arquivo (nesse caso, .gpkg)
+
+    :return: GeoDataFrame produzido com base no arquivo
+    :rtype: geopandas.GeoDataFrame
+    """
 
     try:
         geometria_brasil = gpd.read_file("bcim_2016_21_11_2018.gpkg", layer = "lim_unidade_federacao_a")
