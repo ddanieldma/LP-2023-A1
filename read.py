7f7433174f046c356728a472accce6e4a680d552
guilierm <cguilherme182@gmail.com>
2023-10-13 15:04:41 -0300

    add tratamento exceção modulo read

--- read.py
+++ read.py
@@ -1,15 +1,28 @@
 import pandas as pd
 import geopandas as gpd
 
-def ler_csv(caminho_csv):
-    return pd.read_csv(caminho_csv, encoding='latin-1', sep = ';')
+def ler_csv(caminho_csv): 
 
-"""
-Adicionar tratamento de exceção
-"""
+    try:
+        df = pd.read_csv(caminho_csv, encoding='latin-1', sep = ';')
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+    except ValueError:
+        print("O caminho deve ser uma string")
+    else:
+        return df    
+    
 
 base_inep = ler_csv("ed-superior-inep.csv")
 
+
 def criar_geometria_brasil():
-    geometria_brasil = gpd.read_file("bcim_2016_21_11_2018.gpkg", layer = "lim_unidade_federacao_a")
-    return geometria_brasil
+
+    try:
+        geometria_brasil = gpd.read_file("bcim_2016_21_11_2018.gpkg", layer = "lim_unidade_federacao_a")
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+    except ValueError:
+        print("O caminho deve ser uma string")
+    else:
+        return geometria_brasil
